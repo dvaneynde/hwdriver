3d7af42508aedca2efd33aff4b126637a9499f90
Dirk Vaneynde <dirk@dlvmechanografie.eu>
2016-05-30 23:57:42 +0200

    Refactor.

--- src/test/java/eu/dlvm/domotics/blocks/concrete/TestScreensProtection.java
+++ src/test/java/eu/dlvm/domotics/blocks/concrete/TestScreensProtection.java
@@ -7,17 +7,22 @@ import org.junit.Test;
 
 import eu.dlvm.domotics.actuators.Screen;
 import eu.dlvm.domotics.connectors.AlarmEvent2Screen;
+import eu.dlvm.domotics.connectors.ThresholdEvent2Screen;
 import eu.dlvm.domotics.sensors.IAlarmListener;
+import eu.dlvm.domotics.sensors.IThresholdListener;
 
 public class TestScreensProtection extends TestScreensBase {
 
-	protected AlarmEvent2Screen alarmEvent2Screen;
-	
+	private AlarmEvent2Screen alarmEvent2Screen;
+	private ThresholdEvent2Screen threshold2Screen;
+
 	@Before
 	public void init() {
 		super.init();
 		alarmEvent2Screen = new AlarmEvent2Screen("testAlarm", null);
 		alarmEvent2Screen.registerListener(super.sr);
+		threshold2Screen = new ThresholdEvent2Screen("testLight", null);
+		threshold2Screen.registerListener(super.sr);
 	}
 
 	@Test
@@ -129,6 +134,73 @@ public class TestScreensProtection extends TestScreensBase {
 		assertOpenInRestProtected();			
 	}
 	
+	@Test
+	public void sunnyAndDown_cloudyAndUp() {
+		loop();
+		assertRestAndOpen();
+
+		threshold2Screen.onEvent(null, IThresholdListener.EventType.HIGH);
+		loop();
+		assertDown();	
+		loop(sr.getMotorDnPeriod() * 1000L+10);
+		assertRestAndClosed();
+		loop(10);
+		assertRestAndClosed();
+		
+		threshold2Screen.onEvent(null, IThresholdListener.EventType.LOW);
+		loop();
+		assertUp();
+		loop(sr.getMotorUpPeriod() * 1000L+100);
+		assertRestAndOpen();
+	}
+	
+	@Test
+	public void firstSunnyAndDown_thenProtectAndUp_thenSunnyButNoEffect() {
+		loop();
+		assertRestAndOpen();
+
+		threshold2Screen.onEvent(null, IThresholdListener.EventType.HIGH);
+		loop();
+		assertDown();	
+		loop(sr.getMotorDnPeriod() * 1000L/2);
+		assertDown();
+		
+		alarmEvent2Screen.onEvent(null, IAlarmListener.EventType.ALARM);
+		loop();
+		Assert.assertEquals(Screen.States.DELAY_DOWN_2_UP, sr.getState());
+		Assert.assertFalse(hw.dnRelais);
+		Assert.assertFalse(hw.upRelais);
+		Assert.assertEquals(true, sr.getProtect());
+		loop(Screen.MOTOR_SWITCH_DELAY_PROTECTION+10);	
+		assertUpProtected();	
+		
+		threshold2Screen.onEvent(null, IThresholdListener.EventType.HIGH);
+		loop();
+		assertUpProtected();	
+		loop(sr.getMotorUpPeriod() * 1000L+10);
+		assertOpenInRestProtected();	
+		
+		threshold2Screen.onEvent(null, IThresholdListener.EventType.HIGH);
+		loop();
+		assertOpenInRestProtected();			
+
+		threshold2Screen.onEvent(null, IThresholdListener.EventType.LOW);
+		loop();
+		assertOpenInRestProtected();			
+
+		sr.down();
+		loop();
+		assertOpenInRestProtected();			
+		
+		alarmEvent2Screen.onEvent(null, IAlarmListener.EventType.SAFE);
+		loop();
+		assertRestAndOpen();	
+				
+		threshold2Screen.onEvent(null, IThresholdListener.EventType.HIGH);
+		loop();
+		assertDown();	
+	}
+
 	/*
 	 * Helpers
 	 */
