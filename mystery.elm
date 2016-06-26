30e0410fad0fd69e2d3ab79fb7295d9ea5e623f0
Dirk Vaneynde <dirk@dlvmechanografie.eu>
2016-06-26 21:47:17 +0200

    mystery van Html Msg opgelost, zie mystery.elm ik vermoed dat Msg gewoon type is van messages dat door runtime geproduceerd wordt, daar moet ik nog voorbeeld van vinden

---
+++ mystery.elm
@@ -0,0 +1,22 @@
+import Html exposing (text)
+
+main =
+  -- dit werkt, dus functie met #2 return value: text (toString (storm "3 beaufort"))
+  text (toString (Wind "Hoezee")) -- dit werkt ook, Alarm heeft constructor voor elke mogelijkheid
+
+type Alarm a = Wind a | Licht a | Onbekend
+
+-- DIT IS HET: return value is Alarm String, zoals Http Msg
+storm : String -> Alarm String
+storm a =
+  Wind a
+
+printIt : Alarm String -> String
+printIt b =
+  case b of
+    Wind a ->
+      "Storm " ++ a
+    Licht a ->
+      "Verblind " ++ a
+    Onbekend ->
+      "?"
