a83b1d4061e0cd9562eebe11185531f183057a9c
Dirk Vaneynde <dirk@dlvmechanografie.eu>
2016-07-01 21:13:20 +0200

    Op weg naar post, encode werkt al.

--- main.elm
+++ main.elm
@@ -5,6 +5,7 @@ import Html.Events exposing (onClick, onCheck)
 import Http
 import Task exposing (Task)
 import Json.Decode exposing (Decoder, decodeString, int, float, string, bool, object3, (:=))
+import Json.Encode as Encode
 
 -- Domotics user interface
 
@@ -25,18 +26,19 @@ main =
 
 
 -- MODEL
-type alias Model = { actuators: String, sunLevel: Int, windLevel: Float, robotOn: Bool, errorMsg: String }
+type alias Model = { actuators: String, sunLevel: Int, windLevel: Float, robotOn: Bool, errorMsg: String, test: String }
 
 init : (Model, Cmd Msg)
-init = ( { actuators="Click Status button...", sunLevel=0, windLevel=0.0, robotOn=False, errorMsg="No worries..." }, Cmd.none )
+init = ( { actuators="Click Status button...", sunLevel=0, windLevel=0.0, robotOn=False, errorMsg="No worries...", test="nothing tested" }, Cmd.none )
 
 
 -- UPDATE
-type Msg = CheckActuators | CheckActuatorsOk String | CheckError Http.Error | CheckInfo | CheckInfoOk DecodedInfo
+type Msg = CheckActuators | CheckActuatorsOk String | CheckError Http.Error | CheckInfo | CheckInfoOk DecodedInfo | Test
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
+    Test -> ({model | test = (toString(sendInfoEncoded model))}, Cmd.none)
     CheckActuators -> (model, checkActuatorsCmd)
     CheckActuatorsOk text -> ({ model | actuators = text }, Cmd.none)
     CheckInfo -> (model, checkInfoCmd)
@@ -55,11 +57,23 @@ checkInfoCmd : Cmd Msg
 checkInfoCmd =
   Task.perform CheckError CheckInfoOk (Http.get requestDecoder url)
 
+sendInfoEncoded : Model -> String
+sendInfoEncoded model =
+  let info =
+    Encode.object [ ("robotOn", Encode.bool model.robotOn)]
+  in
+    Encode.encode 4 info
+
+-- hats : Task Error (List String)
+-- hats =
+--     post (list string) "http://example.com/hat-categories.json" empty
 
 -- VIEW
 view : Model -> Html Msg
 view model =
   div [][
+    div [] [ text "Test: " , button [ onClick Test ] [ text "Test"], text model.test],
+    div [][ Html.hr [] [] ],
     div [] [ text "Actuators: ", text model.actuators ],
     div [] [ text "Check Actuators: " , button [ onClick CheckActuators ] [ text "status"]],
     div [][ Html.hr [] [] ],
