dca15153afc89ce1b8484e7ebf0278f0ea1d4071
Dirk Vaneynde <dirk@dlvmechanografie.eu>
2016-07-02 22:05:16 +0200

    Opkuis.

--- main.elm
+++ main.elm
@@ -4,7 +4,7 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onCheck)
 import Http
 import Task exposing (Task)
-import Json.Decode exposing (Decoder, decodeString, int, float, string, bool, object3, (:=))
+import Json.Decode as Decode exposing (Decoder, decodeString, int, float, string, bool, object3, (:=))
 import Json.Encode as Encode
 
 -- Domotics user interface
@@ -15,14 +15,11 @@ import Json.Encode as Encode
 -}
 
 -- GLOBAL
-url : String
+urlBase = "http://localhost:8080/domo/"
 --url = "http://192.168.0.10:8080/domo/actuators"
 --url="http://localhost:8080/domo/actuators"
-url = "http://localhost:8080/domo/screenRobot"
-
-urlPost :  String
-urlPost = "http://localhost:8080/domo/screenRobotUpdate"
-
+urlGetRobotInfo = urlBase ++ "screenRobot"
+urlPostRobotUpdate = urlBase ++ "screenRobotUpdate"
 
 main =
   Html.App.program { init = init, view = view, update = update, subscriptions = (always Sub.none) }
@@ -36,37 +33,36 @@ init = ( { actuators="Click Status button...", sunLevel=0, windLevel=0.0, robotO
 
 
 -- UPDATE
-type Msg = CheckActuators | CheckActuatorsOk String | CheckError Http.Error | CheckErrorRaw Http.RawError| CheckInfo | CheckInfoOk DecodedInfo |
+type Msg = CheckError Http.Error | CheckErrorRaw Http.RawError| CheckInfo | CheckInfoOk ReceivedInfoRecord |
     RobotClick Bool | Test | ShowResult String | ShowResponse Http.Response
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    CheckActuators -> (model, checkActuatorsCmd)
-    CheckActuatorsOk text -> ({ model | actuators = text }, Cmd.none)
-    CheckInfo -> (model, checkInfoCmd)
+    Test -> ({model | test = (toString(updateInfoEncoder model))}, Cmd.none)
+    CheckInfo -> (model, receiveInfoCmd)
     CheckInfoOk info -> ({ model | robotOn = info.robotOn, sunLevel = info.sunLevel, windLevel = info.windLevel }, Cmd.none)
-    CheckError error -> ({ model | actuators = toString error }, Cmd.none)
+    CheckError error -> ({ model | errorMsg = toString error }, Cmd.none)
     CheckErrorRaw error -> ({ model | actuators = toString error }, Cmd.none)
     RobotClick value -> ({ model | robotOn = value}, updateInfoCmd model)
-    Test -> ({model | test = (toString(sendInfoEncoded model))}, Cmd.none)
     ShowResult value -> ({model | test = value}, Cmd.none)
     ShowResponse value -> ({model | test = (toString value)}, Cmd.none)
 
-checkActuatorsCmd : Cmd Msg
-checkActuatorsCmd = Task.perform CheckError CheckActuatorsOk (Http.getString url)
+-- checkActuatorsCmd : Cmd Msg
+-- checkActuatorsCmd = Task.perform CheckError CheckActuatorsOk (Http.getString url)
 
-type alias DecodedInfo = { robotOn : Bool, sunLevel : Int, windLevel : Float }
-requestDecoder : Decoder DecodedInfo
-requestDecoder =
-  object3 DecodedInfo ("robotOn" := bool) ("sunLevel" := int) ("windLevel" := float)
+type alias ReceivedInfoRecord = { robotOn : Bool, sunLevel : Int, windLevel : Float }
 
-checkInfoCmd : Cmd Msg
-checkInfoCmd =
-  Task.perform CheckError CheckInfoOk (Http.get requestDecoder url)
+receivedInfoDecoder : Decoder ReceivedInfoRecord
+receivedInfoDecoder =
+  object3 ReceivedInfoRecord ("robotOn" := bool) ("sunLevel" := int) ("windLevel" := float)
 
-sendInfoEncoded : Model -> String
-sendInfoEncoded model =
+receiveInfoCmd : Cmd Msg
+receiveInfoCmd =
+  Task.perform CheckError CheckInfoOk (Http.get receivedInfoDecoder urlGetRobotInfo)
+
+updateInfoEncoder : Model -> String
+updateInfoEncoder model =
   let info =
     Encode.object [ ("robotOn", Encode.bool model.robotOn)]
   in
@@ -74,25 +70,18 @@ sendInfoEncoded model =
 
 updateInfoCmd : Model -> Cmd Msg
 updateInfoCmd model =
-  Task.perform CheckErrorRaw ShowResponse (updateInfo model)
-
---updateInfo: Model -> Task Http.Error String
---updateInfo model =
-  --Http.post string urlPost (Http.string """{"robotOn": false}""")
-  -- (Http.string ("{ \"robotOn\": " ++ (toString model.robotOn) ++ " }"))
-  -- (Http.string """{ "robotOn": "false" }""")
-  --(Http.string (sendInfoEncoded model))--Http.empty --(Http.string (sendInfoEncoded model))
-updateInfo: Model -> Task Http.RawError Http.Response
-updateInfo model =
-  Http.send Http.defaultSettings
+  let
+    body = Http.string (updateInfoEncoder model)
+    request : Http.Request
+    request =
       { verb = "POST"
-      , headers =
-          [ ( "Content-Type", "application/json" )
-        
-          ]
-      , url = urlPost
-      , body = Http.string (sendInfoEncoded model)
+      , headers = [ ( "Content-Type", "application/json" ) ]
+      , url = urlPostRobotUpdate
+      , body = Http.string (updateInfoEncoder model)
       }
+  in
+    Task.perform CheckErrorRaw ShowResponse (Http.send Http.defaultSettings request)
+
 
 -- VIEW
 view : Model -> Html Msg
@@ -100,11 +89,9 @@ view model =
   div [][
     div [] [ text "Test: " , button [ onClick Test ] [ text "Test"], text model.test],
     div [][ Html.hr [] [] ],
-    div [] [ text "Actuators: ", text model.actuators ],
-    div [] [ text "Check Actuators: " , button [ onClick CheckActuators ] [ text "status"]],
+    div [] [text "Error: ", text model.errorMsg],
     div [][ Html.hr [] [] ],
     div [] [ button [ onClick CheckInfo ] [text "Check..."]],
-    div [] [text "Error: ", text model.errorMsg],
     div [] [ input [ type' "checkbox", checked model.robotOn, onCheck RobotClick ] [], text " zonne-automaat" ],
     div [] [text "Zon: ", meter [ Html.Attributes.min "0", Html.Attributes.max "3800", Html.Attributes.value (toString model.sunLevel) ] [], text ((toString (round (toFloat model.sunLevel/3650.0*100)))++"%") ],
     div [] [text "Wind: ", meter [ Html.Attributes.min "0", Html.Attributes.max "8.5", Html.Attributes.value (toString model.windLevel) ] [], text (toString model.windLevel) ]
