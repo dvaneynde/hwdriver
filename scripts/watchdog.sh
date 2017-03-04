109a3fa815e633af03b9b0c81dede52f9faa6f24
Dirk Vaneynde <dirk@dlvmechanografie.eu>
2017-03-04 22:21:11 +0100

    Fix watchdog.sh.

--- scripts/watchdog.sh
+++ scripts/watchdog.sh
@@ -7,12 +7,12 @@ INITSCRIPT=/etc/init.d/domotic.sh
 checkprocess() {
 	if [[ -f $1 ]]; then
 		pid=$(cat $1)
-		ps -p $pid
+		ps -p $pid >/dev/null
 		if [ $? -ne 0 ]
 		then
 			echo "Found pid file $1 but not its process ${pid}. Will restart domotic after 10 sec sleep."
 			sleep 10
-			echo "Restart...
+			echo "Restart..."
 			sudo $INITSCRIPT restart
 		fi
 	fi
